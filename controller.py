04713d20cb243ab9ca83e8dd877f2b96c5cfcef1
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-01-05 19:14:13 +0000

    View methods added, the terminal window will now show the board and ask for a number from the user

--- controller.py
+++ controller.py
@@ -8,3 +8,10 @@ class Controller():
     
     def main(self):
         self.view.main()
+    
+    def take_input(self, user_input):
+        pass
+    
+if __name__ == "__main__":
+    app = Controller()
+    app.main()
--- view.py
+++ view.py
@@ -1,6 +1,53 @@
 class View():
+    # Class Variables
+
     def __init__(self, controller):
         self.controller = controller
+        
+        self.board = ["#" for range in range(10)]
     
-    def main():
-        pass
+    def main(self):
+        self._update_board()
+        self._ask_input()
+    
+    def _update_board(self, changed_index=0, letter=" "):
+        if self.board[changed_index] in ["O", "X"]:
+            print("Position already filled")
+            return self._ask_input()
+        
+        row = 1
+        board_map = []
+        index = 1
+        break_count_horizontal = 1
+        while row < 4:
+            column = 1
+            break_count_vertical = 1
+            while column < 4:
+                if index == changed_index:
+                    self.board[index] = letter
+                
+                board_map.append(self.board[index])
+                index += 1
+                column += 1
+                if break_count_vertical < 3:
+                    board_map.append("|")
+                    break_count_vertical += 1
+            board_map.append("\n")
+            row += 1
+            if break_count_horizontal < 3:
+                board_map.append("-----\n")
+                break_count_horizontal += 1
+        return print("".join(board_map), end="")
+    
+    def _ask_input(self):
+        n = 1
+        while n == 1:
+            try:
+                user_input = int(input("Enter a number from 1-9: "))
+                if user_input < 1 or user_input > 9:
+                    print("Please enter a number from 1-9")
+                    continue
+                n = 2
+            except (ValueError):
+                print("Please enter a number from 1-9")
+        return self.controller.take_input(user_input)
